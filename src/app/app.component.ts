1fec716a45eb5cb5acdbef0eac60500f79054b2d
iDanteh <105389532+iDanteh@users.noreply.github.com>
2024-07-30 11:43:27 -0600

    Tarjetas con información y paginacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { CardComponent } from './components/card/card.component';
+import { PaginatorComponent } from './components/paginator/paginator.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, CardComponent,PaginatorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
