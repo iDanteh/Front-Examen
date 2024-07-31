807b56bb0fb1bf92eb166cf430c98ba5b64bcab3
iDanteh <105389532+iDanteh@users.noreply.github.com>
2024-07-31 16:20:54 -0600

    Arregla las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { VistaComponent } from './pages/vista/vista.component';
+import { AgregarAnimalComponent } from './modals/agregar-animal/agregar-animal.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
-    {
-        path: 'vista',
-        component: VistaComponent
-    },
+    { path: 'vista', component: VistaComponent },
+    { path: 'agregarAnimal', component: AgregarAnimalComponent },
 ];
+
+// @NgModule({
+//     imports: [RouterModule.forRoot(routes)],
+//     exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -8,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
+
+  constructor(private route: Router){}
 
 }
