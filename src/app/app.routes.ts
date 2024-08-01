2de2cdca62b0ce90d897dbfa820bbed5cc70c21c
iDanteh <105389532+iDanteh@users.noreply.github.com>
2024-07-31 20:03:12 -0600

    Login y register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { VistaComponent } from './pages/vista/vista.component';
 import { AgregarAnimalComponent } from './modals/agregar-animal/agregar-animal.component';
 import { NgModule } from '@angular/core';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 
 export const routes: Routes = [
+    {path: '', redirectTo: '/home' , pathMatch: 'full'},
+    {path: 'home', component: WelcomeComponent},
     { path: 'vista', component: VistaComponent },
     { path: 'agregarAnimal', component: AgregarAnimalComponent },
 ];
---
+++ src/app/pages/welcome/welcome.component.ts
@@ -0,0 +1,89 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-welcome',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+export class WelcomeComponent implements OnInit {
+  authForm!: FormGroup;
+  isLoginMode = true;
+
+  constructor(private fb: FormBuilder, private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  private loginUser(email: string, password: string): Observable<any> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.post<any>('http://127.0.0.1:8000/api/login', { email, password }, { headers });
+  }
+
+  private registerUser(nombre: string, email: string, password: string): Observable<any> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.post<any>('http://127.0.0.1:8000/api/newUser', { nombre, email, password }, { headers });
+  }
+
+
+  private initForm(): void {
+    this.authForm = this.fb.group({
+      nombre: ['', this.isLoginMode ? [] : [Validators.required]], // Campo 'nombre' solo requerido en registro
+      email: ['', [Validators.required, Validators.email]], // Validación para email
+      password: ['', [Validators.required]], // Campo 'password' requerido
+      confirmPassword: ['', this.isLoginMode ? [] : Validators.required] // Campo 'confirmPassword' solo requerido en registro
+    });
+  }
+  
+
+
+  onSwitchMode(): void {
+    this.isLoginMode = !this.isLoginMode;
+    this.initForm();
+  }
+
+  onSubmit(): void {
+    if (!this.authForm.valid) {
+      return;
+    }
+  
+    const nombre = this.authForm.value.nombre;
+    const email = this.authForm.value.email;
+    const password = this.authForm.value.password;
+  
+    if (this.isLoginMode) {
+      // Llamar a la API de inicio de sesión
+      this.loginUser(email, password).subscribe(
+        response => {
+          console.log('Login successful', response);
+        },
+        error => {
+          console.error('Login error', error);
+        }
+      );
+    } else {
+      if (password !== this.authForm.value.confirmPassword) {
+        console.error('Passwords do not match');
+        return;
+      }
+      this.registerUser(nombre, email, password).subscribe(
+        response => {
+          console.log('Registration successful', response);
+        },
+        error => {
+          console.error('Registration error', error);
+        }
+      );
+    }
+    this.authForm.reset();
+  }
+  
+
+}
